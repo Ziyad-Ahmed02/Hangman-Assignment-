5875d9489b0316a21c0f97a6e73ca26a644a90d2
Ziyad-Ahmed02 <ZiyadAhmed@aucegypt.edu>
2020-10-19 20:12:11 +0200

    Last Commit

--- game.cpp
+++ game.cpp
@@ -6,18 +6,20 @@ game::game(QWidget *parent)
     , ui(new Ui::game)
 {
     srand(time(0));
-    ChosenWord = words[rand() % 30];
+    ChosenWord=words[rand()%30];
 
     ui->setupUi(this);
-    for (int i = 0; i < ChosenWord.size(); i++)
+    for(int i=0;i<ChosenWord.size();i++)
     {
-        ui->label->setText(ui->label->text() + '-');
+        ui->label->setText(ui->label->text()+'-');
     }
-    ui->setupUi(this);
-    darkGreen=QPen(Qt::darkGreen);
-    darkBlue=QPen(Qt::darkBlue);
-
-    darkGreen.setWidth(7); darkBlue.setWidth(7);
+       background = new QGraphicsScene(this);
+       ui->graphicsView->setScene(background);
+       QPen green(Qt::green);
+       QPen yellow(Qt::yellow);
+       //QPen orange(Qt::orange);
+       QPen red(Qt::red);
+       green.setWidth(10);yellow.setWidth(10); red.setWidth(10);
 }
 
 game::~game()
@@ -29,25 +31,24 @@ void game::HangmanDrawer()
 {
     switch (HangmanCounter++) // why y & x axis are inverted
         {  //addLine(qreal x1, qreal y1, qreal x2, qreal y2, const QPen &pen = QPen())
-    case 1: base = background-> addLine(-500,2,-350,2,darkGreen);
+    case 1: base = background-> addLine(-500,2,-350,2,green);
         break;
-    case 2: stand = background-> addLine(-425,2,-425,-150,darkGreen);
+    case 2: stand = background-> addLine(-425,2,-425,-150,green);
         break;
-    case 3: hanger = background-> addLine(-425,-150,-300,-150,darkGreen);
+    case 3: hanger = background-> addLine(-425,-150,-300,-150,yellow);
         break;
-    case 4: rope = background-> addLine(-300,-150,-300,-125,darkGreen);
+    case 4: rope = background-> addLine(-300,-150,-300,-125,red);
         break;
-    case 5: face = background-> addEllipse(-315,-125,30,30,darkBlue);
+    case 5: face = background-> addEllipse(-315,-125,30,30,red);
         break;
-    case 6: torso = background-> addLine(-300,-95,-300,-45,darkBlue);
+    case 6: torso = background-> addLine(-300,-95,-300,-45,red);
         break;
-    case 7: arm1 = background-> addLine(-300,-75,-280,-65,darkBlue); arm2 = background-> addLine(-300,-75,-320,-65,darkBlue);
+    case 7: arm1 = background-> addLine(-300,-75,-280,-65,red); arm2 = background-> addLine(-300,-75,-320,-65,red);
         break;
-    case 8: leg1 = background-> addLine(-300,-45,-280,-35,darkBlue); leg2 = background-> addLine(-300,-45,-320,-35,darkBlue);
+    case 8: leg1 = background-> addLine(-300,-45,-280,-35,red); leg2 = background-> addLine(-300,-45,-320,-35,red);
         break;
     }
 }
-
 void game::endgame(QString word)
 {
     //int score = 0, hangMancounter=0, normalLives = 7, hardcoreLives = 60;
@@ -84,4 +85,107 @@ void game::endgame(QString word)
                  exit(0);
             }
         }
-        else if(ChosenMode == "hard
+        else if(ChosenMode == "hardcore")
+        {
+            score++;
+            displayscore();
+            ui->label->setText("");
+           for(int i=0;i<ChosenWord.size();i++)
+           {
+               ui->label->setText(ui->label->text()+'-');
+           }
+            if(score == 20)
+            {
+                msgBox.setText("Congratulations, you won!");
+                   msgBox.exec();
+                 exit(0);
+            }
+        }
+        else if(ChosenMode == "survival")
+        {
+            ui->label->setText("");
+           for(int i=0;i<ChosenWord.size();i++)
+           {
+               ui->label->setText(ui->label->text()+'-');
+           }
+            score++;
+        }
+    }
+    else if(HangmanCounter==9){
+      {    msgBox.setText("you lost");
+            msgBox.exec();
+        exit(0);
+}
+    }
+
+}
+void game::keyPressEvent(QKeyEvent *event)
+{
+    int t=0;
+    for(int i=0;i<ChosenWord.size();i++)
+       { if(ChosenWord.at(i)==event->text())
+           { ui->label->setText(ui->label->text().replace(i,1,event->text()));
+             t=1;
+              endgame(ui->label->text());
+           }
+    if((i==ChosenWord.size()-1)&&(t==0))
+        HangmanDrawer();
+    endgame(ui->label->text());
+}
+
+}
+void game::displayscore()
+{
+    ui->label_2->setText("Score : " + QString::number(score));
+}
+void game::setMode(QString n)
+{
+ChosenMode=n;
+}
+
+
+void game::on_pushButton_2_clicked()
+{
+    ChosenMode="normal";
+    reset();
+}
+
+void game::on_pushButton_3_clicked()
+{
+    ChosenMode="hardcore";
+    reset();
+}
+
+void game::on_pushButton_4_clicked()
+{
+    ChosenMode="survival";
+    reset();
+}
+
+void game::on_pushButton_clicked()
+{
+    ChosenMode="oneWord";
+    reset();
+}
+void game::reset()
+{
+    score=0;
+    HangmanCounter=0;
+    background->clear();
+    ChosenWord=words[rand()%30];
+     ui->label->setText("");
+    for(int i=0;i<ChosenWord.size();i++)
+    {
+        ui->label->setText(ui->label->text()+'-');
+    }
+}
+void game::on_pushButton_5_clicked()
+{
+    reset();
+}
+
+void game::on_pushButton_6_clicked()
+{
+    msgBox.setText("Ziyad Ahmed\nMohammed Fikry\nMalek Elhosainy\nNadia Mahran");
+       msgBox.exec();
+}
--- game.h
+++ game.h
@@ -1,14 +1,16 @@
 #ifndef GAME_H
 #define GAME_H
-
-#include <QMainWindow>
-#include <QDialog>
+#include<QWidget>
 #include <QGraphicsScene>
 #include <QGraphicsEllipseItem>
 #include <QGraphicsLineItem>
 #include <QPen>
 #include <QPainter>
 #include <QGraphicsItem>
+#include <QMainWindow>
+#include<QMessageBox>
+#include<QKeyEvent>
+#include<cstdlib>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class game; }
@@ -21,31 +23,48 @@ class game : public QMainWindow
 public:
     game(QWidget *parent = nullptr);
     ~game();
-    void HangmanDrawer();
-    void endgame(QString);
+void endgame(QString);
+void HangmanDrawer();
+void keyPressEvent(QKeyEvent *event)override;
+void displayscore();
+void setMode(QString);
+private slots:
+
+
+void on_pushButton_clicked();
+
+void on_pushButton_2_clicked();
+
+void on_pushButton_3_clicked();
+
+void on_pushButton_4_clicked();
+void reset();
+
+void on_pushButton_5_clicked();
+
+void on_pushButton_6_clicked();
 
 private:
     Ui::game *ui;
-    QGraphicsScene *background;
-    QGraphicsEllipseItem *face;
-    QGraphicsLineItem *base;
-    QGraphicsLineItem *stand;
-    QGraphicsLineItem *hanger;
-    QGraphicsLineItem *rope;
-    QGraphicsLineItem *torso;
-    QGraphicsLineItem *arm1;
-    QGraphicsLineItem *arm2;
-    QGraphicsLineItem *leg1;
-    QGraphicsLineItem *leg2;
-    QPen darkGreen;
-    QPen yellow;
-    //QPen orange;
-    QPen darkBlue;
-    QPainter paint;
-    int HangmanCounter=0;
-    QString words[30] = { "video","cat","name","banana","four","sale","red","exit","morning", "sky", "pen", "warm","mouse","lamp","orange","zero","head","bottle","friend","box","tiny","quick","jam","sun","light","duck","grey","smart","fan","glass" };
-    QString ChosenWord, ChosenMode = "oneWord";
-    int HangmanCounter = 0, score = 0;
-
+    QString words[30]={"video","cat","name","banana","four","sale","red","exit","morning", "sky", "pen", "warm","mouse","lamp","orange","zero","head","bottle","friend","box","tiny","quick","jam","sun","light","duck","grey","smart","fan","glass" };
+    QString ChosenWord,ChosenMode="oneWord";
+    int HangmanCounter=0,score=0;
+       QGraphicsScene *background;
+       QGraphicsEllipseItem *face;
+       QGraphicsLineItem *base;
+       QGraphicsLineItem *stand;
+       QGraphicsLineItem *hanger;
+       QGraphicsLineItem *rope;
+       QGraphicsLineItem *torso;
+       QGraphicsLineItem *arm1;
+       QGraphicsLineItem *arm2;
+       QGraphicsLineItem *leg1;
+       QGraphicsLineItem *leg2;
+       QPen green;
+       QPen yellow;
+       //QPen orange;
+       QPen red;
+       QPainter paint;
+       QMessageBox msgBox;
 };
 #endif // GAME_H
